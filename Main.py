86733314d5bc352134d9c0d078f969c057568c20
proJ2E <41222120+proJ2E@users.noreply.github.com>
2018-07-15 17:24:32 +0900

    입력한 날짜의 글을 모두 파싱

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import PINAN_POST
 import LOGIN_SESSION
+import Parser
 
 #리퀘스트를 위한 경로
-URL=  "http://ref.comgal.info/sjzb.php?id=cgref"
+URL=  "http://ref.comgal.info/sjzb.php?id=cgref&page="
 writeURL ="http://ref.comgal.info/write_ok.php?id=cgref"
 commentURL = "http://ref.comgal.info/comment_ok.php?id=cgref"
 refURL = "http://ref.comgal.info/write.php?id=cgref"
@@ -10,7 +11,9 @@ logURL = 'http://ref.comgal.info/login_check.php'
 
 #로그인 세션 얻기
 user_id = ''
-password =''
-session = LOGIN_SESSION.Login(logURL, user_id, password)
+password = ''
+#session = LOGIN_SESSION.Login(logURL, user_id, password)
+
+p = Parser.Parsing()
 #글 작성
 
